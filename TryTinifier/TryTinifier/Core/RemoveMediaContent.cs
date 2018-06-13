d2f1404da7f9d933c485d49e555e5ab545d82e7a
SSA\aleksandr.chuikov <aleksandr.chuikov@ssa.group>
2018-06-13 19:18:00 +0300

    Started Task #5109

---
+++ TryTinifier/TryTinifier/Core/RemoveMediaContent.cs
@@ -0,0 +1,44 @@
+﻿using Quartz;
+using Quartz.Impl;
+
+namespace TryTinifier.Core
+{
+    public class RemoveMediaContent
+    {
+        public static void Start()
+        {
+            ISchedulerFactory schedFact = new StdSchedulerFactory();
+
+            IScheduler sched = schedFact.GetScheduler();
+            sched.Start();
+
+            IJobDetail job = JobBuilder.Create<SimpleJob>()
+                    .WithIdentity("job1", "group1")
+                    .Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("trigger1", "group1")
+                .StartNow()
+                .WithSimpleSchedule(x => x.WithIntervalInHours(24).RepeatForever())
+                //.StartAt(DateTimeOffset.Parse("01:00:00 AM"))
+                //.WithSimpleSchedule(x => x.WithIntervalInSeconds(15).RepeatForever())
+                .Build();
+
+            sched.ScheduleJob(job, trigger);
+        }
+    }
+
+    public class SimpleJob : IJob
+    {
+
+        void IJob.Execute(IJobExecutionContext context)
+        {
+            DeleteContentMediaFolder();
+        }
+
+        private void DeleteContentMediaFolder()
+        {
+
+        }
+    }
+}
---
+++ TryTinifier/TryTinifier/Core/UmbracoStartupCore.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using System.Linq;
+using Umbraco.Core;
+using Umbraco.Core.Events;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services;
+
+namespace TryTinifier.Core
+{
+    public class UmbracoStartupCore : ApplicationEventHandler
+    {        
+        private readonly IMediaService _mediaService;
+
+        public UmbracoStartupCore()
+        {
+            _mediaService = ApplicationContext.Current.Services.MediaService;            
+        }
+
+        protected override void ApplicationStarted(UmbracoApplicationBase umbraco, ApplicationContext context)
+        {
+            MediaService.Saving += MediaService_Saving;
+            RemoveMediaContent.Start();
+        }
+
+        private void MediaService_Saving(IMediaService sender, SaveEventArgs<IMedia> e)
+        {
+            CheckImageSize(e);
+            if (GetMediaFolderSize() > 100)
+            {
+                e.CancelOperation(new EventMessage("Stop", "Image limit exceeded! Maximum size of media content is 100MB. Please delete content!", EventMessageType.Error));
+            }
+
+        }
+
+        private void CheckImageSize(SaveEventArgs<IMedia> e)
+        {
+            foreach (var mediaItem in e.SavedEntities)
+            {
+                var size = mediaItem.Properties["umbracoBytes"].Value.ToString();
+                var x = ((float.Parse(size) / 1024f) / 1024f);
+                if (x > 1)
+                {
+                    e.CancelOperation(new EventMessage("Stop", "Maximum image size is 1MB!", EventMessageType.Error));
+                }
+            }
+        }
+
+        private float GetMediaFolderSize()
+        {
+            var pathToMediaFolder = System.Web.HttpContext.Current.Server.MapPath(@"~/media");
+            var foldersCount = Directory.GetFiles(pathToMediaFolder, "*", SearchOption.AllDirectories).Count();
+            float sizeOfMediaFolder = Directory.GetFiles(pathToMediaFolder, "*", SearchOption.AllDirectories).Sum(t => (new FileInfo(t).Length)) / (1024f * 1024f);
+            return sizeOfMediaFolder;
+        }
+    }
+}
